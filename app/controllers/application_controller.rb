4491cabe3d1143f84b65eb5e1751652e06134a27
Aaine Barintuma <abarintuma@gmail.com>
2026-09-06 12:53:56 +0300

    add lauguage options in nav bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  # keeps ?locale=ja in all links automatically as users click around
 end
